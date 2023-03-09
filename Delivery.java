59a0f9af35e555c97993bb23e5cb40493405f6eb
clarkj49 <jamie.clarke49@mail.dcu.ie>
2023-03-09 15:04:40 +0000

    Working on Delivery class

---
+++ Delivery.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.secuirty.Random;
+
+
+public class Delivery
+{
+    static Object Book = new Book();
+    static int ticks = 10;
+    static List <Book> BookList = new ArrayList<Book>();
+    
+
+    public static List<Book> Delivery2()
+    {
+        for(int i = 0; i < ticks; i++)
+        {
+            book.setCategory();
+            BookList.add(book);
+        }
+    }
+
+}
